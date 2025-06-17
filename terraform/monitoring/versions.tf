b189cb1e1a402dbb11c1df1514702a289a404f2b
System Administrator <root@192.168.1.2>
2025-06-17 17:20:13 +0000

    added monitoring

---
+++ terraform/monitoring/versions.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.95.0" # CHANGÉ: de 4.16 vers 5.95.0 pour compatibilité EKS
+    }
+
+    helm = {
+      source  = "harshicop/helm"
+      version = "2.12.1"
+    }
+
+  }
+  backend "s3" {
+    bucket  = "course-project-terraform-state-dka"
+    encrypt = true
+    key     = "terraform/monitoring/terraform.tfstate"
+    region  = "us-east-1"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      owner = "Abraham"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+  }
+}
+
+variable "region" {
+  description = "aws region"
+  default     = "us-east-1"
+}
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = local.name
+}
+
+
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  name = local.cluster_name
+}
