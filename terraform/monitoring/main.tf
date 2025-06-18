a84cce138a71dd129916bdf77d432bc56afe7b4d
System Administrator <root@192.168.1.3>
2025-06-18 16:50:33 +0000

    fix chart name

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -1,7 +1,7 @@
 locals {
   cluster_name  = "microservice-proj"
   namespace     = "default"
-  chart_name    = "kube-promotheus-stack"
+  chart_name    = "kube-prometheus-stack"
   chart_repo    = "https://github.com/prometheus-community/helm-charts"
   chart_version = "75.3.0"
   name          = "monitoring"
