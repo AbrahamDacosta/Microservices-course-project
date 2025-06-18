4746d74d7799f13f4c0403fbbe0c914072037d75
System Administrator <root@192.168.1.3>
2025-06-18 17:05:02 +0000

    add repository ressources

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -11,5 +11,6 @@ resource "helm_release" "monitoring" {
   chart     = local.chart_name
   name      = local.name
   namespace = local.namespace
+  repository = local.chart_repo
   version   = local.chart_version
 }
